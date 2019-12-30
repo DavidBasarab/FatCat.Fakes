95a9bbf5ae1863a1105fe3f7460e394c600ab675
David Basarab <dbasarab617@gmail.com>
2019-12-30 11:48:47 -0500

    test project set up

--- src/FatCat.Fakes.Tests/Class1.cs
+++ src/FatCat.Fakes.Tests/Class1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using FluentAssertions;
+using Xunit;
 
 namespace FatCat.Fakes.Tests
 {
-    public class Class1
-    {
-    }
+	public class Class1
+	{
+		[Fact]
+		public void Test_Name() { true.Should().BeTrue(); }
+	}
 }
