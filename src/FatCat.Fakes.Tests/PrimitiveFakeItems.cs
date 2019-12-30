458a6a76d4993734b382c09c8e7ee2231c0455fd
David Basarab <dbasarab617@gmail.com>
2019-12-30 13:15:47 -0500

    Can fake an int

---
+++ src/FatCat.Fakes.Tests/PrimitiveFakeItems.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+using Xunit;
+
+namespace FatCat.Fakes.Tests
+{
+	public class PrimitiveFakeItems
+	{
+		[Fact]
+		public void AnIntCanBeFaked()
+		{
+			var value = Faker.Create<int>();
+
+			value.Should().BeInRange(int.MinValue, int.MaxValue);
+		}
+
+		[Fact]
+		public void EachIntWillBeRandom()
+		{
+			var previousValue = 0;
+
+			for (var i = 0; i < 13; i++)
+			{
+				var currentValue = Faker.Create<int>();
+
+				currentValue.Should().NotBe(previousValue);
+
+				previousValue = currentValue;
+			}
+		}
+	}
+}
--- src/FatCat.Fakes/Faker.cs
+++ src/FatCat.Fakes/Faker.cs
@@ -1,8 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FatCat.Fakes
 {
-    public class Faker
-    {
-    }
+	internal class FakeFactory
+	{
+		private static readonly Lazy<FakeFactory> instance = new Lazy<FakeFactory>(() => new FakeFactory());
+
+		public static FakeFactory Instance => instance.Value;
+
+		private Dictionary<Type, FakeGenerator> TypeGenerators { get; } = new Dictionary<Type, FakeGenerator>();
+		
+		public object GetValue(Type type)
+		{
+			var fakeType = GetTypeForFake(type);
+
+			return TypeGenerators.TryGetValue(fakeType, out var faker) ? faker.Generate(type) : null;
+		}
+		
+		private Type GetTypeForFake(Type type)
+		{
+			return type;
+
+			// if (!type.IsGenericType) return type;
+			//
+			// var genericType = type.GetGenericTypeDefinition();
+			//
+			// return genericType == typeof(Nullable<>) ? type : genericType;
+		}
+
+		private FakeFactory()
+		{
+			TypeGenerators.Add(typeof(int), new IntGenerator());
+		}
+	}
+
+	internal class IntGenerator : FakeGenerator
+	{
+		public override object Generate(Type type) => Random.Next();
+	}
+
+	internal abstract class FakeGenerator
+	{
+		private static Random random;
+
+		protected static Random Random => random ??= new Random();
+
+		public abstract object Generate(Type type);
+	}
+
+	public static class Faker
+	{
+		private static FakeFactory FakeFactory { get; } = FakeFactory.Instance;
+		
+		public static T Create<T>()
+		{
+			return (T)FakeFactory.GetValue(typeof(T));
+		}
+	}
 }
