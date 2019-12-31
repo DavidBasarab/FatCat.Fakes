d31a4c22c6c404aa727e7609f36d70a09d9ff824
David Basarab <dbasarab617@gmail.com>
2019-12-31 14:38:47 -0500

    Will pick an interface

---
+++ src/FatCat.Fakes.Tests/MultipleImplementationOfInterface.cs
@@ -0,0 +1,57 @@
+using FluentAssertions;
+using Xunit;
+
+namespace FatCat.Fakes.Tests
+{
+	public class MultipleImplementationOfInterface
+	{
+		[Fact]
+		public void WillRandomlyPickAnImplementationOfAnInterface()
+		{
+			var firstFound = false;
+			var secondFound = false;
+			var thirdFound = false;
+
+			for (var i = 0; i < 30; i++)
+			{
+				var item = Faker.Create<IMyInterface>();
+
+				firstFound = firstFound || item is FirstImplementation;
+				secondFound = secondFound || item is SecondImplementation;
+				thirdFound = thirdFound || item is ThirdImplementation;
+			}
+
+			firstFound.Should().BeTrue();
+			secondFound.Should().BeTrue();
+			thirdFound.Should().BeTrue();
+		}
+
+		private class FirstImplementation : IMyInterface
+		{
+			public int SomeNumber { get; set; }
+
+			public string SomeString { get; set; }
+		}
+
+		private interface IMyInterface
+		{
+			public int SomeNumber { get; set; }
+
+			public string SomeString { get; set; }
+		}
+
+		private class SecondImplementation : IMyInterface
+		{
+			public int SomeNumber { get; set; }
+
+			public string SomeString { get; set; }
+		}
+
+		private class ThirdImplementation : IMyInterface
+		{
+			public int SomeNumber { get; set; }
+
+			public string SomeString { get; set; }
+		}
+	}
+}
