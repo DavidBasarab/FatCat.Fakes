d7016f68aa0473390b75e2a224f197860353a772
David Basarab <dbasarab617@gmail.com>
2021-05-15 22:27:30 -0400

    testing issue

--- OneOff/Program.cs
+++ OneOff/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using FatCat.Fakes;
 using FatCat.Fakes.Tests.SpeedUp.Models;
 
@@ -11,13 +12,34 @@ namespace OneOff
 		{
 			Console.WriteLine("Going to test how fast Faker is");
 
-			var watch = Stopwatch.StartNew();
+			var numberOfFakesToCreate = 12;
 
-			var nasaFake = Faker.Create<Nasa>();
-			
-			watch.Stop();
-			
-			Console.WriteLine($"Took <{watch.Elapsed}> to create | TotalMS <{watch.Elapsed.TotalMilliseconds}>");
+			var totalTime = TimeSpan.Zero;
+
+			var tasks = new Task[numberOfFakesToCreate];
+
+			for (var i = 0; i < numberOfFakesToCreate; i++)
+			{
+				tasks[i] = Task.Run(async () =>
+									{
+										var watch = Stopwatch.StartNew();
+
+										var nasaFake = Faker.Create<Nasa>();
+
+										watch.Stop();
+
+										totalTime += watch.Elapsed;
+
+										await Task.CompletedTask;
+									});
+			}
+
+			Task.WaitAll(tasks);
+
+			var avgMs = totalTime.TotalMilliseconds / 12;
+			var avgTime = TimeSpan.FromMilliseconds(avgMs);
+
+			Console.WriteLine($"Number Of Attempts <{numberOfFakesToCreate}> | Total Time {totalTime} | Average Time {avgTime} | Average Ms {avgMs}");
 		}
 	}
 }
