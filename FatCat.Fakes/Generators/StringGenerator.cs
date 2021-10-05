ed52d6989bcfa01943c4670e681b6200bd30dcba
David Basarab <dbasarab617@gmail.com>
2021-10-05 16:39:46 -0400

    trying to prevent dup random characters

--- FatCat.Fakes/Generators/StringGenerator.cs
+++ FatCat.Fakes/Generators/StringGenerator.cs
@@ -5,8 +5,6 @@ namespace FatCat.Fakes.Generators
 {
 	internal class StringGenerator : FakeGenerator
 	{
-		private const string StringCharacters = "TUV345ABCmnoSWXYpqrstuDEFZ01LMNOPQ26789abcdefghijklvwxyzGHIJKR";
-
 		public override object Generate(Type typeToGenerate)
 		{
 			var length = Random.Next(8, 37);
@@ -16,9 +14,9 @@ namespace FatCat.Fakes.Generators
 
 		public string Generate(int length)
 		{
-			
-			
-			return new(Enumerable.Repeat(StringCharacters, length).Select(s => s[Random.Next() % s.Length]).ToArray());
+			var characters = Guid.NewGuid().ToString();
+
+			return new string(Enumerable.Repeat(characters, length).Select(s => s[Random.Next() % s.Length]).ToArray());
 		}
 	}
 }
