3da148e356483d3266b3ed987d53e84e5c6d63ce
David Basarab <dbasarab617@gmail.com>
2023-03-21 21:42:29 -0400

    added DateTime to static Fake method

--- FatCat.Fakes/Generators/DateTimeGenerator.cs
+++ FatCat.Fakes/Generators/DateTimeGenerator.cs
@@ -2,28 +2,28 @@ using System;
 
 namespace FatCat.Fakes.Generators
 {
-	internal class DateTimeGenerator : FakeGenerator
+internal class DateTimeGenerator : FakeGenerator
+{
+	public override object Generate(Type typeToGenerate)
 	{
-		public override object Generate(Type typeToGenerate)
-		{
-			var now = DateTime.Now;
+		var now = DateTime.Now;
 
-			var randomDays = Random.Next(-1500, 7500);
+		var randomDays = Random.Next(-1500, 7500);
 
-			now = now.AddDays(randomDays);
+		now = now.AddDays(randomDays);
 
-			now = AddRandomTimeSpans(now);
+		now = AddRandomTimeSpans(now);
 
-			return now.AddDays(randomDays);
-		}
-
-		private static DateTime AddRandomTimeSpans(DateTime now)
-		{
-			var timeSpanGenerator = new TimespanGenerator();
-
-			for (var i = 0; i < Random.Next(3, 11); i++) now = now + (TimeSpan)timeSpanGenerator.Generate(typeof(TimeSpan));
-
-			return now;
-		}
+		return now.AddDays(randomDays);
 	}
+
+	private static DateTime AddRandomTimeSpans(DateTime now)
+	{
+		var timeSpanGenerator = new TimespanGenerator();
+
+		for (var i = 0; i < Random.Next(3, 11); i++) now += (TimeSpan)timeSpanGenerator.Generate(typeof(TimeSpan));
+
+		return now;
+	}
+}
 }
