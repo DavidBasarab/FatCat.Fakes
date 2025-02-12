63e171e10b1a22af1fa23cbbb21ecffddbdc8721
David Basarab <dbasarab617@gmail.com>
2025-02-11 20:28:33 -0500

    can fake a list of records

--- FatCat.Fakes.Tests/CanFakeARecord.cs
+++ FatCat.Fakes.Tests/CanFakeARecord.cs
@@ -7,10 +7,28 @@ public class CanFakeARecord
     {
         var record = Faker.Create<RecordToFake>();
 
+        VerifyRecord(record);
+    }
+
+    private static void VerifyRecord(RecordToFake record)
+    {
         record.Should().NotBeNull();
 
         record.Number.Should().BeInRange(int.MinValue, int.MaxValue);
         record.Name.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public void CanFakeAListOfRecords()
+    {
+        var records = Faker.Create<List<RecordToFake>>(length: 4);
+
+        records.Count.Should().Be(4);
+
+        foreach (var record in records)
+        {
+            VerifyRecord(record);
+        }
     }
 
     private record RecordToFake(int Number, string Name) { }
