faa808dfa38ad1166a2c1e468f0177e762f83433
David Basarab <dbasarab617@gmail.com>
2025-02-11 20:01:17 -0500

    got a basic thing to work in one off

--- OneOff/Program.cs
+++ OneOff/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using FatCat.Toolkit.Console;
+using Newtonsoft.Json;
 
 namespace OneOff;
 
@@ -33,6 +37,126 @@ public class ItemTwo : BaseItem
     // public int ItemTwoProperty { get; set; }
 }
 
+public class DynamicRecordCreator
+{
+    public static object CreateRecord(Type recordType, Dictionary<string, object> properties)
+    {
+        if (properties == null || properties.Count == 0)
+        {
+            throw new ArgumentException("At least one property is required.");
+        }
+
+        var assemblyName = recordType.Assembly.GetName();
+
+        var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(
+            assemblyName,
+            AssemblyBuilderAccess.Run
+        );
+
+        var moduleBuilder = assemblyBuilder.DefineDynamicModule(recordType.Name + "Module");
+
+        var typeBuilder = moduleBuilder.DefineType(
+            recordType.Name,
+            TypeAttributes.Public | TypeAttributes.Sealed,
+            typeof(object)
+        );
+
+        var fields = new List<FieldBuilder>();
+        var propertyBuilders = new List<PropertyBuilder>();
+        var constructorParamTypes = new List<Type>();
+
+        foreach (var property in properties)
+        {
+            var propertyName = property.Key;
+            var propertyType = property.Value.GetType();
+
+            // Define private field
+            var fieldBuilder = typeBuilder.DefineField(
+                "_" + propertyName,
+                propertyType,
+                FieldAttributes.Private
+            );
+
+            fields.Add(fieldBuilder);
+
+            // Define property
+            var propertyBuilder = typeBuilder.DefineProperty(
+                propertyName,
+                PropertyAttributes.HasDefault,
+                propertyType,
+                null
+            );
+
+            propertyBuilders.Add(propertyBuilder);
+
+            // Define getter
+            var getMethodBuilder = typeBuilder.DefineMethod(
+                "get_" + propertyName,
+                MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig,
+                propertyType,
+                Type.EmptyTypes
+            );
+
+            var getIL = getMethodBuilder.GetILGenerator();
+            getIL.Emit(OpCodes.Ldarg_0);
+            getIL.Emit(OpCodes.Ldfld, fieldBuilder);
+            getIL.Emit(OpCodes.Ret);
+
+            // Define setter
+            var setMethodBuilder = typeBuilder.DefineMethod(
+                "set_" + propertyName,
+                MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig,
+                null,
+                new[] { propertyType }
+            );
+
+            var setIL = setMethodBuilder.GetILGenerator();
+            setIL.Emit(OpCodes.Ldarg_0);
+            setIL.Emit(OpCodes.Ldarg_1);
+            setIL.Emit(OpCodes.Stfld, fieldBuilder);
+            setIL.Emit(OpCodes.Ret);
+
+            // Attach getter and setter to property
+            propertyBuilder.SetGetMethod(getMethodBuilder);
+            propertyBuilder.SetSetMethod(setMethodBuilder);
+
+            // Collect constructor parameters
+            constructorParamTypes.Add(propertyType);
+        }
+
+        // Define constructor
+        var constructorBuilder = typeBuilder.DefineConstructor(
+            MethodAttributes.Public,
+            CallingConventions.Standard,
+            constructorParamTypes.ToArray()
+        );
+
+        var ctorIL = constructorBuilder.GetILGenerator();
+        ctorIL.Emit(OpCodes.Ldarg_0);
+        ctorIL.Emit(OpCodes.Call, typeof(object).GetConstructor(Type.EmptyTypes)); // Call base constructor
+
+        var index = 1;
+
+        foreach (var field in fields)
+        {
+            ctorIL.Emit(OpCodes.Ldarg_0);
+            ctorIL.Emit(OpCodes.Ldarg, index++);
+            ctorIL.Emit(OpCodes.Stfld, field);
+        }
+
+        ctorIL.Emit(OpCodes.Ret);
+
+        // Create type
+        var dynamicType = typeBuilder.CreateType();
+
+        // Instantiate and return the new record
+        var constructorArgs = new object[properties.Count];
+        properties.Values.CopyTo(constructorArgs, 0);
+
+        return Activator.CreateInstance(dynamicType, constructorArgs);
+    }
+}
+
 public record TestingRecord(int Number, string Name);
 
 internal class Program
@@ -47,6 +171,13 @@ internal class Program
             {
                 ConsoleLog.WriteMagenta($" - {prop.Name} == {prop.PropertyType.FullName}");
             }
+
+            var item = DynamicRecordCreator.CreateRecord(
+                recordType,
+                new Dictionary<string, object>() { { "Number", 42 }, { "Name", "The Answer" } }
+            );
+
+            ConsoleLog.WriteGreen($"{JsonConvert.SerializeObject(item)}");
         }
         catch (Exception ex)
         {
