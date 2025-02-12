d08ddd539911721d0b20af1257809cc56323bab4
David Basarab <dbasarab617@gmail.com>
2025-02-11 20:32:02 -0500

    formatting

--- FatCat.Fakes.Tests/CanFakeARecord.cs
+++ FatCat.Fakes.Tests/CanFakeARecord.cs
@@ -10,18 +10,10 @@ public class CanFakeARecord
         VerifyRecord(record);
     }
 
-    private static void VerifyRecord(RecordToFake record)
-    {
-        record.Should().NotBeNull();
-
-        record.Number.Should().BeInRange(int.MinValue, int.MaxValue);
-        record.Name.Should().NotBeNullOrEmpty();
-    }
-
     [Fact]
     public void CanFakeAListOfRecords()
     {
-        var records = Faker.Create<List<RecordToFake>>(length: 4);
+        var records = Faker.Create<List<RecordToFake>>(4);
 
         records.Count.Should().Be(4);
 
@@ -29,6 +21,14 @@ public class CanFakeARecord
         {
             VerifyRecord(record);
         }
+    }
+
+    private static void VerifyRecord(RecordToFake record)
+    {
+        record.Should().NotBeNull();
+
+        record.Number.Should().BeInRange(int.MinValue, int.MaxValue);
+        record.Name.Should().NotBeNullOrEmpty();
     }
 
     private record RecordToFake(int Number, string Name) { }
